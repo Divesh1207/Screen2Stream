a95c79ac8f0322caca318f5675c6fb08530f665c
Divesh <diveshp904@gmail.com>
2024-09-18 17:17:44 +0530

    configured vite config js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
     server: {
       proxy: {
-        '/api': env.VITE_API_BASE_URL
-      }
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
     },
-    plugins: [react()]
-  }
-})
+    plugins: [react()],
+  };
+});
